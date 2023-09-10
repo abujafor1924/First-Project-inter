2720ecf398da544b724dc3955919f2f5e55311af
abu-jafor <abujaforwd@yahoo.com>
2023-09-10 11:47:32 +0600

    Add Loading

--- src/components/routes/Loading.jsx
+++ src/components/routes/Loading.jsx
@@ -1,39 +1,63 @@
 import { useEffect, useState } from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { ScaleLoader } from "react-spinners";
+// import { ScaleLoader } from "react-spinners";
 
+// eslint-disable-next-line react/prop-types
 const Loading = ({ children }) => {
-  const [loading, setLoading] = useState();
-
+  const [isLoading, setIsLoading] = useState(false);
+  const location = useLocation();
   useEffect(() => {
-    // onAuthStateChanged come fierbase
-    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      // console.log(currentUser);
-      setLoading(false);
-    });
-    // stop obserbing
-    return () => {
-      return unSubscribe();
-    };
-  }, []);
+    setIsLoading(true);
 
-  const loacation = useLocation();
-  if (loading) {
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(loadingTimeout);
+  }, [location]);
+
+  if (isLoading) {
     return (
-      <div className=" h-[70vh] flex flex-col justify-center items-center ">
-        <ScaleLoader height={100} width={4} color="#07332F" />
+      <div className="mt-20 md:w-96 mx-auto">
+        <div className=" h-[70vh] flex flex-col justify-center items-center ">
+          <ScaleLoader height={100} width={4} color="#07332F" />
+        </div>
       </div>
     );
   }
-  // use user
-  if (user) {
-    return children;
-  }
 
-  return (
-    <Navigate to={"/login"} state={{ from: loacation }} replace></Navigate>
-  );
+  return children;
+
+  // useEffect(() => {
+  //   // onAuthStateChanged come fierbase
+  //   const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+  //     setUser(currentUser);
+  //     // console.log(currentUser);
+  //     setLoading(false);
+  //   });
+  //   // stop obserbing
+  //   return () => {
+  //     return unSubscribe();
+  //   };
+  // }, []);
+
+  // const loacation = useLocation();
+  // if (loading) {
+  //   return (
+  //     <div className=" h-[70vh] flex flex-col justify-center items-center ">
+  //       <ScaleLoader height={100} width={4} color="#07332F" />
+  //     </div>
+  //   );
+  // }
+  // // use user
+  // if (user) {
+  //   return children;
+  // }
+
+  // return (
+  //   <Navigate to={"/login"} state={{ from: loacation }} replace></Navigate>
+  // );
 };
 
 export default Loading;
--- src/components/routes/router.jsx
+++ src/components/routes/router.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../Home/Home/Home";
 import About from "../Home/About/About";
 import Contact from "../Home/Contact/Contact";
+import Loading from "./Loading";
 
 export const router = createBrowserRouter([
   {
@@ -12,15 +13,27 @@ export const router = createBrowserRouter([
       {
         path: "/",
 
-        element: <Home />,
+        element: (
+          <Loading>
+            <Home />
+          </Loading>
+        ),
       },
       {
         path: "/about",
-        element: <About />,
+        element: (
+          <Loading>
+            <About />
+          </Loading>
+        ),
       },
       {
         path: "/contact",
-        element: <Contact />,
+        element: (
+          <Loading>
+            <Contact />
+          </Loading>
+        ),
       },
     ],
   },
