dabed2a3273096663be7c4b03b8592c6b7ded955
abu-jafor <abujaforwd@yahoo.com>
2023-09-10 09:19:16 +0600

    Router Loading

--- src/components/routes/Loading.jsx
+++ src/components/routes/Loading.jsx
@@ -1,49 +1,40 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { ScaleLoader } from "react-spinners";
 
-// eslint-disable-next-line react/prop-types
+
 const Loading = ({ children }) => {
   const [loading, setLoading] = useState();
-  //   const { user, loding } = useContext(AutContext);
-  const location = useLocation();
-  console.log(location);
-  useEffect(() => {
-    const exmLoading = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
 
-    return () => clearTimeout(exmLoading);
+  useEffect(() => {
+    // onAuthStateChanged come fierbase
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      // console.log(currentUser);
+      setLoading(false);
+    });
+    // stop obserbing
+    return () => {
+      return unSubscribe();
+    };
   }, []);
 
+  const loacation = useLocation();
   if (loading) {
     return (
       <div className=" h-[70vh] flex flex-col justify-center items-center ">
         <ScaleLoader height={100} width={4} color="#07332F" />
       </div>
     );
-  } else {
+  }
+  // use user
+  if () {
     return children;
   }
 
-  // this is use Authsystem
-
-  // const { user, loading } = useAuth();
-  // const loacation = useLocation();
-  //   if (loading) {
-  //     return (
-  //       <div className=" h-[70vh] flex flex-col justify-center items-center ">
-  //         <ScaleLoader height={100} width={4} color="#07332F" />
-  //       </div>
-  //     );
-  //   }
-  //   if (user) {
-  //     return children;
-  //   }
-
-  //   return (
-  //     <Navigate to={"/login"} state={{ from: loacation }} replace></Navigate>
-  //   );
+  return (
+    <Navigate to={"/login"} state={{ from: loacation }} replace></Navigate>
+  );
 };
 
 export default Loading;
